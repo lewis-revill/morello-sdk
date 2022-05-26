9c971d710190f0510b6afc3404a8f7b29a792ffc
Vincenzo Frascino <vincenzo.frascino@arm.com>
2022-05-26 15:37:58 +0100

    morello: Modify init to start heartbeat
    Modify init to start the heartbeat process.
    
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@arm.com>

--- morello/projects/init/init.c
+++ morello/projects/init/init.c
@@ -6,12 +6,19 @@
 #include <unistd.h>
 #include <sys/mount.h>
 #include <sys/stat.h>
-#include <sys/stat.h>
+#include <sys/wait.h>
 
 char *const env[] = {
 	"PATH=/usr/sbin:/bin:/sbin",
 	NULL,
 };
+
+const char *process[] = {
+	"/sbin/heartbeat",
+	NULL,
+};
+
+const char *shell = "/bin/sh";
 
 void fs_mount(const char *src, const char *dst,
 	      const char *fs_type, unsigned long mount_flags,
@@ -35,8 +42,56 @@ void fs_mount(const char *src, const char *dst,
 	}
 }
 
+int exec_cmd(const char *cmd)
+{
+	pid_t c_pid;
+
+	c_pid = fork();
+
+	if(c_pid == 0)
+	{
+		execle(cmd, cmd, NULL, env);
+
+		printf("[MORELLO]: Cannot exec %s\n", cmd);
+		exit(0);
+	}
+
+	return 0;
+}
+
+int exec_shell(const char *shell)
+{
+	pid_t c_pid;
+	int c_status;
+
+	c_pid = fork();
+
+	if(c_pid == 0)
+	{
+		execle(shell, shell, NULL, env);
+
+		printf("[MORELLO]: Cannot exec %s\n", shell);
+		exit(0);
+	} else {
+		pid_t t_pid;
+
+		do {
+			t_pid = wait(&c_status);
+
+			if (t_pid != c_pid)
+				printf("[MORELLO]: Exit process: %d\n", t_pid);
+		} while(t_pid != c_pid);
+
+		return c_status;
+	}
+}
+
 int main(int argc, char *argv[]) {
+	int index = 0;
+	int status = 0;
+
 #ifndef MORELLO_DOCKER
+	/* Mount filesystems */
 	fs_mount("none", "/proc", "proc", 0, "");
 	fs_mount("none", "/dev/pts", "devpts", 0, "");
 	fs_mount("none", "/dev/mqueue", "mqueue", 0, "");
@@ -45,9 +100,30 @@ int main(int argc, char *argv[]) {
 	fs_mount("none", "/sys/fs/cgroup", "cgroup", 0, "");
 #endif
 
+	/* Set hostname */
 	sethostname("morello", sizeof("morello"));
 
-	execle("/bin/sh", "/bin/sh", NULL, env);
-	perror("Cannot exec /bin/sh");
-	return 1;
+	printf("Welcome to Morello PCuABI environment (busybox)!\n");
+	printf("Have a lot of fun!\n\n");
+
+	/* Start Processes */
+	while(process[index] != NULL)
+	{
+		printf("[MORELLO]: Starting %s...\n", process[index]);
+		status = exec_cmd(process[index]);
+
+		index++;
+	}
+
+	/* Start Shell */
+	while(1)
+	{
+		printf("[MORELLO]: Starting %s...\n", shell);
+		status = exec_shell(shell);
+
+		sleep(1);
+	}
+
+	/* We should never reach this point */
+	return status;
 }
