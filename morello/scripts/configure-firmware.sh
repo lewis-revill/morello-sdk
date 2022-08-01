cc7844da24a1da961c49c2ee7fd049f600078c78
Vincenzo Frascino <vincenzo.frascino@arm.com>
2022-08-01 16:01:46 +0100

    morello: Add linux to submodules
    Add the linux kernel to the supported submodules.
    
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@arm.com>

--- morello/scripts/configure-firmware.sh
+++ morello/scripts/configure-firmware.sh
@@ -9,12 +9,12 @@ CURR_DIR=$(pwd)
 submodule_update() {
 	echo "$1 updating progress..."
 	# Populate repositories
-	git submodule update --init --recursive --progress projects/bsp/$1
-	git submodule update --remote --merge projects/bsp/$1
+	git submodule update --init --recursive --progress $1
+	git submodule update --remote --merge $1
 }
 
-submodule_update_morello() {
-	submodule_update $1
+submodule_update_bsp() {
+	submodule_update projects/bsp/$1
 
 	cd projects/bsp/$1
 	git checkout -b morello/release-$2 origin/morello/release-$2
@@ -22,7 +22,7 @@ submodule_update_morello() {
 }
 
 submodule_update_open() {
-	submodule_update $1
+	submodule_update projects/bsp/$1
 
 	cd projects/bsp/$1
 	git checkout -b morello/master origin/master
@@ -30,21 +30,35 @@ submodule_update_open() {
 	cd ${CURR_DIR}
 }
 
-PROJECTS_LIST=( arm-tf scp uefi )
-PROJECTS_OPEN=( grub mbedtls )
-PROJECTS_OPEN_HASH=( a53e530f8ad3770c3b03c208c08ae4162f68e3b1 523f0554b6cdc7ace5d360885c3f5bbcc73ec0e8 )
+submodule_update_projects() {
+	submodule_update projects/$1
+
+	cd projects/$1
+	git checkout -b morello/master origin/master
+	cd ${CURR_DIR}
+}
+
+PROJECTS_LIST=( linux )
+PROJECTS_BSP_LIST=( arm-tf scp uefi )
+PROJECTS_BSP_OPEN=( grub mbedtls )
+PROJECTS_BSP_OPEN_HASH=( a53e530f8ad3770c3b03c208c08ae4162f68e3b1 523f0554b6cdc7ace5d360885c3f5bbcc73ec0e8 )
 INDEX=0
 
 if [ ! -f "${CURR_DIR}/.firmware-env" ]; then
-	for i in "${PROJECTS_LIST[@]}"
+	for i in "${PROJECTS_BSP_LIST[@]}"
 	do
-		submodule_update_morello $i $MORELLO_FIRMWARE_VERSION
+		submodule_update_bsp $i $MORELLO_FIRMWARE_VERSION
 	done
 
-	for i in "${PROJECTS_OPEN[@]}"
+	for i in "${PROJECTS_BSP_OPEN[@]}"
 	do
-		submodule_update_open $i ${PROJECTS_OPEN_HASH[$INDEX]}
+		submodule_update_open $i ${PROJECTS_BSP_OPEN_HASH[$INDEX]}
 		let INDEX++
+	done
+
+	for i in "${PROJECTS_LIST[@]}"
+	do
+		submodule_update_projects $i
 	done
 
 	touch ${CURR_DIR}/.firmware-env
