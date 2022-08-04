1d68e45ab0cfb9f8642e255eb5de4cd7e6985a5b
Vincenzo Frascino <vincenzo.frascino@arm.com>
2022-08-04 10:54:57 +0100

    morello: Add a clear interface to build subprojects
    Add a clear interface to build subprojects.
    
    Subprojects supported:
     - firmware
     - linux
     - c-apps
     - rootfs
     - docker
    
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@arm.com>

--- morello/scripts/build-all.sh
+++ morello/scripts/build-all.sh
@@ -5,8 +5,6 @@
 MODE="aarch64"
 LIBSHIM="--disable-libshim"
 DEV_MODE="off"
-DOCKER="off"
-BUILD_LIB="off"
 CURR_DIR=$(pwd)
 MORELLO_PROJECTS=$(realpath $(pwd)/projects)
 BSP_HOME=$(realpath $(pwd)/bsp)
@@ -43,9 +41,22 @@ export MORELLO_DOCKER
 export MODE
 export LIBSHIM
 export DEV_MODE
-export DOCKER
-export BUILD_LIB
 export _NCORES
+
+# Optional configuration paramenters
+OPTIONS_FIRMWARE="off"
+OPTIONS_LINUX="off"
+OPTIONS_C_APPS="off"
+OPTIONS_ROOTFS="off"
+OPTIONS_DOCKER="off"
+OPTIONS_BUILD_LIB="off"
+
+export OPTIONS_FIRMWARE
+export OPTIONS_LINUX
+export OPTIONS_C_APPS
+export OPTIONS_ROOTFS
+export OPTIONS_DOCKER
+export OPTIONS_BUILD_LIB
 
 help () {
 cat <<EOF
@@ -56,6 +67,10 @@ OPTIONS:
   --x86_64            build on an x86_64 host
   --enable-libshim    enable libshim in musl
   --dev               experimental mode (allows to use more recent versions of musl)
+  --firmware          generate the firmware for Morello
+  --linux             builds linux for Morello
+  --c-apps            builds example c applications for Morello
+  --rootfs            builds the rootfs for Morello
   --docker            generate a busybox based docker image
   --build-lib         build libraries from source (e.g. compiler_rt, crtobjects...)
   --help              this help message
@@ -72,8 +87,12 @@ main () {
 		--x86_64) MODE="x86_64" ;;
 		--enable-libshim) LIBSHIM="--enable-libshim" ;;
 		--dev) DEV_MODE="on" ;;
-		--docker) DOCKER="on";;
-		--build-lib) BUILD_LIB="on";;
+		--firmware) OPTIONS_FIRMWARE="on" ;;
+		--linux) OPTIONS_LINUX="on" ;;
+		--c-apps) OPTIONS_C_APPS="on" ;;
+		--rootfs) OPTIONS_ROOTFS="on" ;;
+		--docker) OPTIONS_DOCKER="on";;
+		--build-lib) OPTIONS_BUILD_LIB="on";;
 		--help|-h) help ;;
 	esac
 	done
@@ -92,12 +111,6 @@ main () {
 	# Create required directories
 	mkdir -p ${MORELLO_ROOTFS} && mkdir -p ${MORELLO_TESTING} && mkdir -p ${GCC_HOME}
 
-	# Configure GCC Toolchain
-	${CURR_DIR}/scripts/configure-gcc-toolchain.sh
-
-	# Configure Firmware for Morello
-	${CURR_DIR}/scripts/configure-firmware.sh
-
 	# Configure LLVM and musl for Morello
 	${CURR_DIR}/scripts/configure-llvm-musl.sh
 
@@ -106,60 +119,74 @@ main () {
 	make
 	cd ${CURR_DIR}
 
-	# Build SCP
-	${CURR_DIR}/scripts/build-scp.sh
-
-	# Build UEFI (UEFI must be built before then arm-tf)
-	${CURR_DIR}/scripts/build-uefi.sh
-
-	# Build ARM-TF
-	${CURR_DIR}/scripts/build-arm-tf.sh
-
-	# Generate Firmware
-	${CURR_DIR}/scripts/generate-firmware.sh
-
-	# Build GRUB
-	${CURR_DIR}/scripts/build-grub.sh
-
-	# Build Linux
-	${CURR_DIR}/scripts/build-linux.sh
-
 	# Build Musl
 	${CURR_DIR}/scripts/build-musl.sh
 
-	if [ "$BUILD_LIB" = "on" ]; then
-	# Build Libraries
+	if [ "$OPTIONS_FIRMWARE" = "on" ]; then
+		# Configure GCC Toolchain
+		${CURR_DIR}/scripts/configure-gcc-toolchain.sh
+
+		# Configure Firmware for Morello
+		${CURR_DIR}/scripts/configure-firmware.sh
+
+		# Build SCP
+		${CURR_DIR}/scripts/build-scp.sh
+
+		# Build UEFI (UEFI must be built before then arm-tf)
+		${CURR_DIR}/scripts/build-uefi.sh
+
+		# Build ARM-TF
+		${CURR_DIR}/scripts/build-arm-tf.sh
+
+		# Generate Firmware
+		${CURR_DIR}/scripts/generate-firmware.sh
+
+		# Build GRUB
+		${CURR_DIR}/scripts/build-grub.sh
+	fi
+
+	if [ "$OPTIONS_LINUX" = "on" ]; then
+		# Build Linux
+		${CURR_DIR}/scripts/build-linux.sh
+	fi
+
+	if [ "$OPTIONS_BUILD_LIB" = "on" ]; then
+		# Build Libraries
 		${CURR_DIR}/scripts/build-libraries.sh
 	fi
 
-	# Create examples/bin
-	mkdir -p ${EXAMPLES_BIN}
+	if [ "$OPTIONS_C_APPS" = "on" ]; then
+		# Create examples/bin
+		mkdir -p ${EXAMPLES_BIN}
 
-	# Build test-app
-	cd ${CURR_DIR}/examples/test-app
-	make
+		# Build test-app
+		cd ${CURR_DIR}/examples/test-app
+		make
 
-	# Build morello-heap-app
-	cd ${CURR_DIR}/examples/morello-heap-app
-	make
+		# Build morello-heap-app
+		cd ${CURR_DIR}/examples/morello-heap-app
+		make
 
-	# Build morello-stack-app
-	cd ${CURR_DIR}/examples/morello-stack-app
-	make
+		# Build morello-stack-app
+		cd ${CURR_DIR}/examples/morello-stack-app
+		make
 
-	# Build morello-pthread-app
-	cd ${CURR_DIR}/examples/morello-pthread-app
-	make
+		# Build morello-pthread-app
+		cd ${CURR_DIR}/examples/morello-pthread-app
+		make
 
-	# Build morello-auxv-app
-	cd ${CURR_DIR}/examples/morello-auxv-app
-	make
+		# Build morello-auxv-app
+		cd ${CURR_DIR}/examples/morello-auxv-app
+		make
+	fi
 
-	# Build PCuABI busybox
-	${CURR_DIR}/scripts/build-busybox.sh
+	if [ "$OPTIONS_ROOTFS" = "on" ]; then
+		# Build PCuABI busybox
+		${CURR_DIR}/scripts/build-busybox.sh
+	fi
 
-	if [ "$DOCKER" = "on" ]; then
-	# Build PCuABI busybox based docker image
+	if [ "$OPTIONS_DOCKER" = "on" ]; then
+		# Build PCuABI busybox based docker image
 		${CURR_DIR}/scripts/build-busybox-docker.sh
 	fi
 }
