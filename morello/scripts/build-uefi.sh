29fc49bcea65c163ae35424adacc3bcf7b758a8c
Vincenzo Frascino <vincenzo.frascino@arm.com>
2022-08-31 13:02:10 +0100

    morello: Update uefi script
    Update uefi build script to comply with the common
    script interface.
    
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@arm.com>

--- morello/scripts/build-uefi.sh
+++ morello/scripts/build-uefi.sh
@@ -9,45 +9,54 @@ export IASL_PREFIX="${MORELLO_PROJECTS}/bsp/acpica/generate/unix/bin/"
 export PACKAGES_PATH="${MORELLO_PROJECTS}/bsp/edk2-platforms:${MORELLO_PROJECTS}/bsp/edk2-non-osi"
 export PYTHON_COMMAND="python3"
 
-_NCORES=$(nproc --all)
+uefi_output_generate() {
+	local platforms=( Soc Fvp )
 
-platforms=( Soc Fvp )
+	# Create output folders
+	output=( soc fvp out )
 
-# Create output folders
-output=( soc fvp out )
+	for o in "${output[@]}"
+	do
+		mkdir -p ${BSP_HOME}/$o/uefi
+	done
+}
 
-for o in "${output[@]}"
-do
-	mkdir -p ${BSP_HOME}/$o/uefi
-done
+uefi_build() {
+	local _NCORES=$(nproc --all)
+	local platforms=( Soc Fvp )
 
-# Build uefi
-options=(
-	-a AARCH64
-	-s
-	-b RELEASE
-	-D ENABLE_MORELLO_CAP
-	-t CLANG35
-)
+	uefi_output_generate
 
-# Make acpica and BaseTools
-make -C "${MORELLO_PROJECTS}/bsp/acpica" -j${_NCORES} iasl
-make -C "${MORELLO_PROJECTS}/bsp/uefi/BaseTools" -j${_NCORES}
-
-cd "${MORELLO_PROJECTS}/bsp/uefi/"
-source ./edksetup.sh --reconfig
-
-for p in "${platforms[@]}"
-do
-	options_plat=(
-		-p "${MORELLO_PROJECTS}/bsp/edk2-platforms/Platform/ARM/Morello/MorelloPlatform$p.dsc"
+	# Build uefi
+	options=(
+		-a AARCH64
+		-s
+		-b RELEASE
+		-D ENABLE_MORELLO_CAP
+		-t CLANG35
 	)
 
-	BaseTools/BinWrappers/PosixLike/build "${options[@]}" "${options_plat[@]}"
+	# Make acpica and BaseTools
+	make -C "${MORELLO_PROJECTS}/bsp/acpica" -j${_NCORES} iasl
+	make -C "${MORELLO_PROJECTS}/bsp/uefi/BaseTools" -j${_NCORES}
 
-	if [ "$p" == "Fvp" ]; then
-		cp "${MORELLO_PROJECTS}/bsp/uefi/Build/morellofvp/RELEASE_CLANG35/FV/BL33_AP_UEFI.fd" "${BSP_HOME}/fvp/uefi"
-	else
-		cp "${MORELLO_PROJECTS}/bsp/uefi/Build/morellosoc/RELEASE_CLANG35/FV/BL33_AP_UEFI.fd" "${BSP_HOME}/soc/uefi"
-	fi
-done
+	cd "${MORELLO_PROJECTS}/bsp/uefi/"
+	source ./edksetup.sh --reconfig
+
+	for p in "${platforms[@]}"
+	do
+		options_plat=(
+			-p "${MORELLO_PROJECTS}/bsp/edk2-platforms/Platform/ARM/Morello/MorelloPlatform$p.dsc"
+		)
+
+		BaseTools/BinWrappers/PosixLike/build "${options[@]}" "${options_plat[@]}"
+
+		if [ "$p" == "Fvp" ]; then
+			cp "${MORELLO_PROJECTS}/bsp/uefi/Build/morellofvp/RELEASE_CLANG35/FV/BL33_AP_UEFI.fd" "${BSP_HOME}/fvp/uefi"
+		else
+			cp "${MORELLO_PROJECTS}/bsp/uefi/Build/morellosoc/RELEASE_CLANG35/FV/BL33_AP_UEFI.fd" "${BSP_HOME}/soc/uefi"
+		fi
+	done
+}
+
+uefi_build $@
