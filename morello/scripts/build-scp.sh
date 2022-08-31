0d9c1162f9d03cafe1a7bd2a1c3afce80302262f
Vincenzo Frascino <vincenzo.frascino@arm.com>
2022-08-31 12:53:54 +0100

    morello: Update scp script
    Update scp build script to comply with the common
    script interface.
    
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@arm.com>

--- morello/scripts/build-scp.sh
+++ morello/scripts/build-scp.sh
@@ -2,57 +2,69 @@
 
 # SPDX-License-Identifier: BSD-3-Clause
 
-GCC_ARM32_PATH=$(which ${GCC_ARM32}gcc)
-OBJCOPY_ARM32_PATH=$(which ${GCC_ARM32}objcopy)
+scp_output_generate() {
+	# Create output folders
+	local output=( soc fvp out )
 
-_NCORES=$(nproc --all)
+	for o in "${output[@]}"
+	do
+		mkdir -p ${BSP_HOME}/$o/scp
+	done
+}
 
-platforms=( soc fvp )
+scp_platforms_generate() {
+	local platforms=( soc fvp )
 
-# Create output folders
-output=( soc fvp out )
+	# Copy firmware across
+	for p in "${platforms[@]}"
+	do
+		cp "${BSP_HOME}/out/scp/scp_romfw/bin/morello-bl1.bin" "${BSP_HOME}/$p/scp/scp-rom.bin"
+		cp "${BSP_HOME}/out/scp/mcp_romfw/bin/morello-mcp-bl1.bin" "${BSP_HOME}/$p/scp/mcp-rom.bin"
+		cp "${BSP_HOME}/out/scp/scp_ramfw_$p/bin/morello-$p-bl2.bin" "${BSP_HOME}/$p/scp/scp-ram.bin"
+		cp "${BSP_HOME}/out/scp/mcp_ramfw_$p/bin/morello-$p-mcp-bl2.bin" "${BSP_HOME}/$p/scp/mcp-ram.bin"
+	done
+}
 
-for o in "${output[@]}"
-do
-	mkdir -p ${BSP_HOME}/$o/scp
-done
+scp_build() {
+	local GCC_ARM32_PATH=$(which ${GCC_ARM32}gcc)
+	local OBJCOPY_ARM32_PATH=$(which ${GCC_ARM32}objcopy)
 
-# Build scp
-options=(
-	-S "${MORELLO_PROJECTS}/bsp/scp"
-	-DSCP_TOOLCHAIN:STRING="GNU"
-	-DCMAKE_C_COMPILER="${GCC_ARM32_PATH}"
-	-DCMAKE_ASM_COMPILER="${GCC_ARM32_PATH}"
-	-DCMAKE_OBJCOPY="${OBJCOPY_ARM32_PATH}"
-	-DCMAKE_BUILD_TYPE=Release
-	-DSCP_LOG_LEVEL=INFO
-)
+	local _NCORES=$(nproc --all)
 
-projects=(
-	mcp_romfw
-	scp_romfw
-	scp_ramfw_fvp
-	mcp_ramfw_fvp
-	scp_ramfw_soc
-	mcp_ramfw_soc
-)
+	scp_output_generate
 
-for fw in "${projects[@]}"
-do
-	options+=(
-		-B "${BSP_HOME}/out/scp/$fw"
-		-DSCP_FIRMWARE_SOURCE_DIR:PATH="morello/$fw"
+	# Build scp
+	options=(
+		-S "${MORELLO_PROJECTS}/bsp/scp"
+		-DSCP_TOOLCHAIN:STRING="GNU"
+		-DCMAKE_C_COMPILER="${GCC_ARM32_PATH}"
+		-DCMAKE_ASM_COMPILER="${GCC_ARM32_PATH}"
+		-DCMAKE_OBJCOPY="${OBJCOPY_ARM32_PATH}"
+		-DCMAKE_BUILD_TYPE=Release
+		-DSCP_LOG_LEVEL=INFO
 	)
 
-	cmake "${options[@]}"
-	cmake --build "${BSP_HOME}/out/scp/$fw" --parallel "$_NCORES"
-done
+	projects=(
+		mcp_romfw
+		scp_romfw
+		scp_ramfw_fvp
+		mcp_ramfw_fvp
+		scp_ramfw_soc
+		mcp_ramfw_soc
+	)
 
-# Copy firmware across
-for p in "${platforms[@]}"
-do
-	cp "${BSP_HOME}/out/scp/scp_romfw/bin/morello-bl1.bin" "${BSP_HOME}/$p/scp/scp-rom.bin"
-	cp "${BSP_HOME}/out/scp/mcp_romfw/bin/morello-mcp-bl1.bin" "${BSP_HOME}/$p/scp/mcp-rom.bin"
-	cp "${BSP_HOME}/out/scp/scp_ramfw_$p/bin/morello-$p-bl2.bin" "${BSP_HOME}/$p/scp/scp-ram.bin"
-	cp "${BSP_HOME}/out/scp/mcp_ramfw_$p/bin/morello-$p-mcp-bl2.bin" "${BSP_HOME}/$p/scp/mcp-ram.bin"
-done
+	for fw in "${projects[@]}"
+	do
+		options+=(
+			-B "${BSP_HOME}/out/scp/$fw"
+			-DSCP_FIRMWARE_SOURCE_DIR:PATH="morello/$fw"
+		)
+
+		cmake "${options[@]}"
+		cmake --build "${BSP_HOME}/out/scp/$fw" --parallel "$_NCORES"
+	done
+
+	scp_platforms_generate
+}
+
+scp_build $@
