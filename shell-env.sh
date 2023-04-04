bac1496b9a5dcb5e288c394997bf0f1df4613728
Vincenzo Frascino <vincenzo.frascino@arm.com>
2023-04-04 08:50:14 +0100

    morello: Add all permissions to the workspace directory
    Add all permissions to the workspace directory.
    
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@arm.com>

--- shell-env.sh
+++ shell-env.sh
@@ -3,5 +3,8 @@
 # Chmod /home/morello/workspace as morello:morello
 chown morello:morello /home/morello/workspace
 
+# Add all permissions to /home/morello/workspace
+chmod 777 /home/morello/workspace
+
 # Run bash to keep container alive
 tail -f /dev/null
