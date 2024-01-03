6ef35fc6b8c02ab95f2a7a9df4d4e30ade7ee912
Lewis Revill <lewis.revill@embecosm.com>
2024-01-03 13:35:01 +0000

    Build libunwind with libraries

--- morello/scripts/build-libraries.sh
+++ morello/scripts/build-libraries.sh
@@ -4,6 +4,7 @@
 
 libraries_clean() {
 	rm -fr ${COMPILER_RT_BIN}
+    rm -fr ${LIBUNWIND_BIN}
 }
 
 libraries_build() {
@@ -61,6 +62,41 @@ libraries_build() {
 
 	mv lib/linux/libclang_rt.builtins-aarch64.a \
 		${CLANG_RESOURCE_DIR}/lib/linux/libclang_rt.builtins-morello.a
+
+    # Build libunwind
+
+	LIBUNWIND_TOOLCHAIN=${LIBUNWIND_BIN}/toolchain.cmake
+
+	mkdir -p ${LIBUNWIND_BIN}
+
+	# Generate toolchain.cmake
+	morello_rt_toolchain aarch64 ${LIBUNWIND_TOOLCHAIN}
+
+	cd ${LIBUNWIND_BIN}
+
+	cmake -Wno-dev \
+		-DCMAKE_TOOLCHAIN_FILE=${LIBUNWIND_TOOLCHAIN} \
+		-DCMAKE_BUILD_TYPE=Release \
+        -DLIBUNWIND_ENABLE_CROSS_UNWINDING=ON \
+        -DLIBUNWIND_TARGET_TRIPLE=aarch64-linux-musl_purecap \
+        -DLIBUNWIND_SYSROOT=${MUSL_BIN} \
+        -DCMAKE_ASM_FLAGS="--target=aarch64-linux-musl_purecap -march=morello+c64 -mabi=purecap" \
+		-DCMAKE_C_FLAGS="--target=aarch64-linux-musl_purecap -march=morello+c64 -mabi=purecap -isystem ${MUSL_BIN}/include" \
+		-DCMAKE_CXX_FLAGS="--target=aarch64-linux-musl_purecap -march=morello+c64 -mabi=purecap -isystem ${MUSL_BIN}/include" \
+		-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+		-DLLVM_TARGETS_TO_BUILD="AArch64" \
+		-DBUILD_SHARED_LIBS=ON \
+		-DCMAKE_SKIP_BUILD_RPATH=OFF \
+		-DCMAKE_INSTALL_RPATH=\$ORIGIN/../lib \
+		-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
+		-DLLVM_ENABLE_ASSERTIONS=ON \
+		-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DLIBUNWIND_ENABLE_SHARED=OFF \
+		../${LLVM_PROJECT}/libunwind
+
+	make unwind
+
+    mv lib/libunwind.a ${MUSL_BIN}/lib/libunwind.a
 }
 
 libraries_build $@
